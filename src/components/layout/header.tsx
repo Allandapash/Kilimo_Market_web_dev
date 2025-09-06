de3aae0aff2599151fd8f4fccc9ca571d028c1a1
Allan Dapash <allandapash@gmail.com>
2025-09-06 10:16:24 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Leaf, Bell, Menu, ShoppingCart, LogOut, UserCircle } from 'lucide-react';
+import { Leaf, Bell, Menu, ShoppingCart, LogOut } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
